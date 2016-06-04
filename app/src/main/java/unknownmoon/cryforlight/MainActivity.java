c07074e53caaa4a7157fde39e83275a77232c2b6
Unknown Moon <unknownmoon@163.com>
2016-06-05 05:13:53 +0800

    Hide app name in action bar

--- app/src/main/java/unknownmoon/cryforlight/MainActivity.java
+++ app/src/main/java/unknownmoon/cryforlight/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
 
         toolbar.setLogo(R.mipmap.ic_launcher);
 
