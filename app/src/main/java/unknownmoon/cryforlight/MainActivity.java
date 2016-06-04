243a681489e5d1e7bf170c0fcbd4b417f90ce946
Unknown Moon <unknownmoon@163.com>
2016-06-05 04:43:56 +0800

    Add basic service toggle button

--- app/src/main/java/unknownmoon/cryforlight/MainActivity.java
+++ app/src/main/java/unknownmoon/cryforlight/MainActivity.java
@@ -5,11 +5,13 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    private Boolean mIsServiceOn = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +20,20 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        toolbar.setLogo(R.mipmap.ic_launcher);
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        fab.setActivated(mIsServiceOn);
+    }
+
+    public void onServiceButtonToggled(View view) {
+        mIsServiceOn = !mIsServiceOn;
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setActivated(mIsServiceOn);
+
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
     }
 
     @Override
