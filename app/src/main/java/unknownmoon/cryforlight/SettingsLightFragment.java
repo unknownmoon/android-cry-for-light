ba4b0ef2f0e7638514ccfdd3a46b73bac13bd458
Unknown Moon <unknownmoon@163.com>
2016-06-06 00:52:11 +0800

    Logging out when shared preferences changed.

--- app/src/main/java/unknownmoon/cryforlight/SettingsLightFragment.java
+++ app/src/main/java/unknownmoon/cryforlight/SettingsLightFragment.java
@@ -2,14 +2,16 @@ package unknownmoon.cryforlight;
 
 
 import android.app.Fragment;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
+import android.util.Log;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class SettingsLightFragment extends PreferenceFragment {
+public class SettingsLightFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
 
 
     public SettingsLightFragment() {
@@ -21,5 +23,30 @@ public class SettingsLightFragment extends PreferenceFragment {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.preferences_light);
+    }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        getPreferenceScreen().getSharedPreferences()
+                .registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        getPreferenceScreen().getSharedPreferences()
+                .unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if (key.equals("pref_light")) {
+            // TODO
+            Log.d("Light", String.format("%s: %d", key, sharedPreferences.getInt(key, getResources().getInteger(R.integer.pref_light_def_val))));
+        }
     }
 }
--- app/src/main/java/unknownmoon/cryforlight/SettingsSoundsFragment.java
+++ app/src/main/java/unknownmoon/cryforlight/SettingsSoundsFragment.java
@@ -2,14 +2,16 @@ package unknownmoon.cryforlight;
 
 
 import android.app.Fragment;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
+import android.util.Log;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class SettingsSoundsFragment extends PreferenceFragment {
+public class SettingsSoundsFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
 
 
     public SettingsSoundsFragment() {
@@ -22,6 +24,30 @@ public class SettingsSoundsFragment extends PreferenceFragment {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.preferences_sounds);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        getPreferenceScreen().getSharedPreferences()
+                .registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        getPreferenceScreen().getSharedPreferences()
+                .unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if (key.equals("pref_sound_level")) {
+            // TODO
+            Log.d("Sound L", String.format("%s: %d", key, sharedPreferences.getInt(key, getResources().getInteger(R.integer.pref_sound_level_def_val))));
+        }
     }
 
 }
