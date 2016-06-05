a2390fcac2739599dc867b28c5f7cb19a2cc42ce
Unknown Moon <unknownmoon@163.com>
2016-06-06 01:14:44 +0800

    Add sound file selection skeleton.

--- app/src/main/java/unknownmoon/cryforlight/SettingsSoundsFragment.java
+++ app/src/main/java/unknownmoon/cryforlight/SettingsSoundsFragment.java
@@ -2,7 +2,9 @@ package unknownmoon.cryforlight;
 
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.media.RingtoneManager;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
 import android.util.Log;
@@ -47,7 +49,21 @@ public class SettingsSoundsFragment extends PreferenceFragment implements Shared
         if (key.equals("pref_sound_level")) {
             // TODO
             Log.d("Sound L", String.format("%s: %d", key, sharedPreferences.getInt(key, getResources().getInteger(R.integer.pref_sound_level_def_val))));
+        } else if (key.equals("pref_sound_file")) {
+            // TODO
+            Log.d("Sound F", sharedPreferences.getString(key, "Not found."));
         }
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        try {
+            Log.d("Sound P", String.format("REQ: %d\nRES: %d\nDATA: %s", requestCode, resultCode, data.getParcelableExtra(RingtoneManager.EXTRA_RINGTONE_PICKED_URI).toString()));
+        } catch (Exception e) {
+            // simply ignore for the moment.
+            Log.d("Sound P", String.format("REQ: %d\nRES: %d", requestCode, resultCode));
+        }
+    }
 }
