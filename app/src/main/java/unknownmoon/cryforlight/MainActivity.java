99268439824a16cc5b30ef7441796e4cfd913954
Unknown Moon <unknownmoon@163.com>
2016-06-05 17:42:49 +0800

    Clean up & re-format

--- app/src/main/java/unknownmoon/cryforlight/MainActivity.java
+++ app/src/main/java/unknownmoon/cryforlight/MainActivity.java
@@ -2,13 +2,9 @@ package unknownmoon.cryforlight;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
