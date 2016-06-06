d8bcc803779efe9ea573907fd1086a8f86599270
Unknown Moon <unknownmoon@163.com>
2016-06-06 21:22:15 +0800

    Add blank service

---
+++ app/src/main/java/unknownmoon/cryforlight/LightService.java
@@ -0,0 +1,16 @@
+package unknownmoon.cryforlight;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class LightService extends Service {
+    public LightService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
